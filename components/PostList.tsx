d6bee3c0d68b8bfe8ede58fd47f568999ca36e1b
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2023-08-30 16:02:51 +0200

    fix confilct

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,47 +1,37 @@
-'use client';
+import React from 'react'; // Import React
 import { useAppSelector } from '@/redux/hooks';
 import { Post } from '@prisma/client';
 import _ from 'lodash';
 import PostCard from './PostCard';
 
-export default async function PostList() {
-  const repo = new PostRepository();
+export default function PostList() {
+	const posts = useAppSelector((state) => state.postReducer.posts);
 
-  const posts = await getPosts(repo);
+	const chunkedPosts = _.chunk(posts, 4);
 
-  function chunkArray(array: FomratedPost[], chunkSize: number) {
-    const result = [];
-    for (let i = 0; i < array.length; i += chunkSize) {
-      result.push(array.slice(i, i + chunkSize));
-    }
-    return result;
-  }
-
-  const chunkedPosts = chunkArray(posts, 4);
-
-  return (
-    <div>
-		{
-			_.isEmpty(posts) ? (
+	return (
+		<div>
+			{_.isEmpty(posts) ? (
 				<div className='text-center text-2xl text-neutral-600 font-semibold'>
 					No posts found
 				</div>
-				:chunkedPosts.map((postChunk, index) => (
+			) : (
+				chunkedPosts.map((postChunk: any, index) => (
 					<div
-					  key={index}
-					  className={`flex ${
-						postChunk.length < 4 ? "justify-center" : "justify-between"
-					  }`}
+						key={index}
+						className={`flex ${
+							postChunk.length < 4 ? 'justify-center' : 'justify-between'
+						}`}
 					>
-					  {postChunk.map((post Post) => (
-						<PostCard key={post.id} post={post} />
-					  ))}
+						{postChunk.map((post: Post) => (
+							<PostCard
+								key={post.id}
+								post={post}
+							/>
+						))}
 					</div>
-				  ))
-				
-		}
-
-        
-    </div>
-  );
+				))
+			)}
+		</div>
+	);
 }
