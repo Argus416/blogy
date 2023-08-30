c4687222ec34b0134f11e1b8521c0f38a507bd6a
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2023-08-30 15:47:12 +0200

    load post

--- app/post/[postId]/page.tsx
+++ app/post/[postId]/page.tsx
@@ -1,26 +1,42 @@
+'use client';
 import DetailedPostView from '@/components/DetailedPostView';
 import { PostRepositoryClient } from '@/gateways/client/PostRepositoryClient';
 import { getPostById } from '@/use-cases/Post';
+import { Post } from '@prisma/client';
 import Link from 'next/link';
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useMemo, useState } from 'react';
 
 type PostDetailsProps = {
 	params: { postId: string };
 };
 
-export default async function PostDetails({ params }: PostDetailsProps) {
-	const repo = new PostRepositoryClient();
-	const post = await getPostById(repo, params.postId);
+export default function PostDetails({ params }: PostDetailsProps) {
+	const repo = useMemo(() => {
+		return new PostRepositoryClient();
+	}, []);
 
+	const [post, setPost] = useState<Post | null>(null);
+
+	useEffect(() => {
+		const fetchPost = async () => {
+			const post = await getPostById(repo, params.postId);
+			setPost(post);
+		};
+		fetchPost();
+	}, [params.postId, repo]);
+
+	const test = async () => {
+		const post = await getPostById(repo, params.postId);
+		console.log({ post });
+	};
 	if (!post) {
 		return <div>Post not found</div>;
 	}
 	return (
 		<div>
 			<Link href='/'>go home</Link>
-			<Suspense fallback={<div>Loading...</div>}>
-				<DetailedPostView post={post} />
-			</Suspense>
+
+			<DetailedPostView post={post} />
 		</div>
 	);
 }
