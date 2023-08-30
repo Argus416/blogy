dbc0cdedc611e8d4b9ee35ef3b9db5008870cd99
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2023-08-30 11:50:20 +0200

    install redux

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import PostList from '@/components/PostList';
+import { wrapper } from '@/store';
+import { Provider } from '@radix-ui/react-toast';
 import Link from 'next/link';
 import { Suspense } from 'react';
 
-export default function Home() {
+function Home() {
 	return (
 		<div>
 			<PostList />
 		</div>
 	);
 }
+
+export default wrapper.withRedux(Home);
