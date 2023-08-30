be78f5aabb0358ac6033daacefa33d6d0dcc0a30
agomesdacosta3 <caps92@live.fr>
2023-08-30 15:48:42 +0200

    add bg and front homepage and detail page

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,23 +1,40 @@
-import { getPosts } from '@/use-cases/Post';
-import PostCard from './PostCard';
-import { PostRepository } from '@/gateways/PostRepository';
-import { Suspense } from 'react';
+import { getPosts } from "@/use-cases/Post";
+import PostCard from "./PostCard";
+import { PostRepository } from "@/gateways/PostRepository";
+import { Suspense } from "react";
+import { FomratedPost } from "@/entities/Post";
 
 export default async function PostList() {
-	const repo = new PostRepository();
+  const repo = new PostRepository();
 
-	const posts = await getPosts(repo);
+  const posts = await getPosts(repo);
 
-	return (
-		<div>
-			<Suspense fallback={<div>Loading...</div>}>
-				{posts.map((post) => (
-					<PostCard
-						key={post.id}
-						post={post}
-					/>
-				))}
-			</Suspense>
-		</div>
-	);
+  function chunkArray(array: FomratedPost[], chunkSize: number) {
+    const result = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
+  }
+
+  const chunkedPosts = chunkArray(posts, 4);
+
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        {chunkedPosts.map((postChunk, index) => (
+          <div
+            key={index}
+            className={`flex ${
+              postChunk.length < 4 ? "justify-center" : "justify-between"
+            }`}
+          >
+            {postChunk.map((post: FomratedPost) => (
+              <PostCard key={post.id} post={post} />
+            ))}
+          </div>
+        ))}
+      </Suspense>
+    </div>
+  );
 }
