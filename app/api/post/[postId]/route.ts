87e774ae95ff89820af4bd8ed31110b214f80b3b
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2023-08-30 16:38:53 +0200

    delete post

--- app/api/post/[postId]/route.ts
+++ app/api/post/[postId]/route.ts
@@ -1,5 +1,5 @@
 import { PostRepositoryServer } from '@/gateways/server/PostRepositoryServer';
-import { getPostById } from '@/use-cases/Post';
+import { deletePost, getPostById } from '@/use-cases/Post';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request, { params }: { params: { postId: string } }) {
@@ -7,6 +7,18 @@ export async function GET(request: Request, { params }: { params: { postId: stri
 		const repo = new PostRepositoryServer();
 
 		const data = await getPostById(repo, params.postId);
+
+		return NextResponse.json(data, { status: 200 });
+	} catch (err: any) {
+		return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+	}
+}
+
+export async function DELETE(request: Request, { params }: { params: { postId: string } }) {
+	try {
+		const repo = new PostRepositoryServer();
+
+		const data = await deletePost(repo, params.postId);
 
 		return NextResponse.json(data, { status: 200 });
 	} catch (err: any) {
--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,11 +1,62 @@
 import { Post } from '@prisma/client';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { Button } from './ui/button';
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from './ui/dialog';
+import { PostRepositoryClient } from '@/gateways/client/PostRepositoryClient';
+import { deletePost } from '@/use-cases/Post';
 
 type PostCardProps = {
 	post: Post;
 };
 
+const DeletePostModal = ({ postId }: { postId: string }) => {
+	const router = useRouter();
+	const repo = new PostRepositoryClient();
+	const deleteHandler = async () => {
+		await deletePost(repo, postId);
+
+		router.push('/');
+	};
+
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button variant='destructive'>Delete</Button>
+			</DialogTrigger>
+
+			<DialogContent className='sm:max-w-[425px]'>
+				<DialogHeader>
+					<DialogTitle>Edit profile</DialogTitle>
+					<DialogDescription>
+						Are you sure you want to delete this post?
+					</DialogDescription>
+				</DialogHeader>
+
+				<DialogFooter>
+					<Button
+						type='submit'
+						variant={'destructive'}
+						onClick={deleteHandler}
+					>
+						Confirm
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+};
+
 export default function PostCard({ post }: PostCardProps) {
+	const router = useRouter();
+
 	return (
 		<div className='p-4 w-full md:w-1/2 lg:w-1/4 mb-12 border border-indigo-300 rounded-lg shadow-md mx-4'>
 			<div className='mt-4 text-center'>
@@ -15,12 +66,20 @@ export default function PostCard({ post }: PostCardProps) {
 				<h2 className='title-font text-lg font-medium text-gray-900 mt-3'>
 					{post.title}
 				</h2>
-				<Link
-					className='block mx-auto text-center bg-indigo-900 text-white px-3 py-1 rounded-md w-1/2 mt-4'
-					href={`/post/${post.id}`}
-				>
-					View
-				</Link>
+				<div className='flex  gap-2  items-center justify-center'>
+					<Button
+						className='bg-indigo-900 hover:bg-indigo-900/75 text-white'
+						onClick={() => router.push(`/post/${post.id}`)}
+					>
+						View
+					</Button>
+
+					<Button>Edit</Button>
+					<DeletePostModal postId={post.id} />
+					{/* <Button className='bg-red-500 text-white hover:bg-red-500/75'>
+						Delete
+					</Button> */}
+				</div>
 			</div>
 		</div>
 	);
