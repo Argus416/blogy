31e65a5341b3d6d43ce4a1f40204352d49b41705
Mohamad Alkhatib <alkhatib.m804@gmail.com>
2023-08-31 10:19:04 +0200

    update details

--- components/DetailedPostView.tsx
+++ components/DetailedPostView.tsx
@@ -7,8 +7,8 @@ type DetailedPostViewProps = {
 export default function DetailedPostView({ post }: DetailedPostViewProps) {
 	return (
 		<div>
-			<h1 className="text-center text-neutral-600 text-2xl font-semi-bold mt-10">Post Title : {post!.title}</h1>
-			<p className="text-center text-neutral-600 text-base font-light mt-12">{post!.content}</p>
+			<h1 className=' text-2xl font-bold mt-10'>{post!.title}</h1>
+			<p className=' text-base font-light mt-12'>{post!.content}</p>
 		</div>
 	);
 }
